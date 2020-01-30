4f188588e44795ae232e05d9dda4a1ad73b13fd4
castillo05 <jorgeantonio20142014@gmail.com>
2020-01-30 07:46:30 -0600

    integrando middleware de accesso

--- src/app.js
+++ src/app.js
@@ -6,6 +6,15 @@ const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+// Cors
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+	res.header('Access-Control-Allow-Headers','Authorization, X-API-KEY,Origin,X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request');
+	res.header('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, DELETE');
+	res.header('Allow','GET, POST, OPTIONS, PUT, DELETE');
+	next();
+})
+
 app.use('/api', userRoute);
 
 app.use('/',(req, res)=>{
